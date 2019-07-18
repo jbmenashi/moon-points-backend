27881e762900c295c9474cdfb2ac622fd106ce5f
Jacob <jbmenashi@gmail.com>
2019-07-18 13:50:14 -0400

    add model structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,68 @@
-import express from 'express';
+
 import cors from 'cors';
 import 'dotenv/config';
+import uuidv4 from 'uuid/v4';
+import bodyParser from 'body-parser';
+import express from 'express';
+import models from './models';
 
 const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-   res.send('Hello World!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+   req.context = {
+      models,
+      me: models.users[1],
+   };
+   next();
+});
+
+app.get('/session', (req, res) => {
+   return res.send(req.context.models.users[req.context.me.id]);
+});
+
+app.get('/users', (req, res) => {
+   return res.send(Object.values(req.context.models.users));
+});
+
+app.get('/users/:userId', (req, res) => {
+   return res.send(req.context.models.users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+   return res.send(Object.values(req.context.models.messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+   return res.send(req.context.models.messages[req.params.messageId]);
+});
+
+app.post('/messages', (req, res) => {
+   const id = uuidv4();
+   const message = {
+      id,
+      text: req.body.text,
+      userId: req.context.me.id,
+   };
+
+   req.context.models.messages[id] = message;
+
+   return res.send(message);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+   const {
+      [req.params.messageId]: message,
+      ...otherMessages
+   } = req.context.models.messages;
+
+   req.context.models.messages = otherMessages;
+
+   return res.send(message);
 });
 
 app.listen(process.env.PORT, () =>
