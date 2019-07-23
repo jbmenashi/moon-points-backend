ffeded6cc20a5fddaeec3fbd394e34b49c2216a3
Jacob <jbmenashi@gmail.com>
2019-07-23 11:00:20 -0400

    add post to user router

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,15 +3,24 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const users = await req.context.models.User.find();
-  return res.send(users);
+   const users = await req.context.models.User.find();
+   return res.send(users);
 });
 
 router.get('/:userId', async (req, res) => {
-  const user = await req.context.models.User.findById(
-    req.params.userId,
-  );
-  return res.send(user);
+   const user = await req.context.models.User.findById(
+      req.params.userId,
+   );
+   return res.send(user);
+});
+
+router.post('/', async (req, res) => {
+   const user = await req.context.models.User.create({
+      username: req.body.username,
+      bio: req.body.bio
+   });
+
+   return res.send(point);
 });
 
 export default router;
